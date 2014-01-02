b01864736b724729fc7f5c7b951dd4b085c64af7
ReedCopsey <reed@reedcopsey.com>
2014-01-01 16:07:31 -0800

    Adding contracts

---
+++ src/FSharp.ViewModule.Core/Contracts.fs
@@ -0,0 +1,25 @@
+﻿namespace FSharp.ViewModule
+
+open System.ComponentModel
+open System.Windows.Input
+
+/// <summary>Extension of ICommand with a public method to fire the CanExecuteChanged event</summary>
+/// <remarks>This type should provide a constructor which accepts an Execute (obj -> unit) and CanExecute (obj -> bool) function</remarks>
+type INotifyCommand =
+    inherit ICommand 
+    
+    /// Trigger the CanExecuteChanged event for this specific ICommand
+    abstract RaiseCanExecuteChanged : unit -> unit
+
+
+/// <summary>Extension of INotifyPropertyChanged with a public method to fire the PropertyChanged event</summary>
+/// <remarks>This type should provide a constructor which accepts an Execute (obj -> unit) and CanExecute (obj -> bool) function</remarks>
+type IViewModel<'a> =
+    inherit INotifyPropertyChanged
+    inherit INotifyDataErrorInfo
+
+    /// Trigger the PropertyChanged event for this specific ViewModel
+    abstract RaisePropertyChanged : string -> unit
+
+    /// Value used to notify view that an asynchronous operation is executing
+    abstract Executing : bool
---
+++ src/FSharp.ViewModule.Core/IViewModuleTypeSpecification.fs
@@ -0,0 +1,28 @@
+﻿namespace FSharp.ViewModule
+
+open Microsoft.FSharp.Quotations
+open System.Windows.Input
+open System.ComponentModel.DataAnnotations
+open System.ComponentModel
+
+/// The Specification used by the type provider to generate a view model
+type IViewModuleTypeSpecification =
+    /// The type used for the View Model.
+    abstract ViewModelType : System.Type
+    /// <summary>A quotation used to build the method to raise PropertyChanged on the ViewModel. </summary>
+    /// <remarks>This can typically be written as <c>&lt;@ ViewModelBase().RaisePropertyChanged("") @&gt;</c></remarks>
+    abstract RaisePropertyChangedCallQuotation : Expr<unit>
+    
+    /// The type used for implementing ICommand
+    abstract CommandType : System.Type
+    /// <summary>A quotation used to build the method to raise CanExecuteChanged on a Command. </summary>
+    /// <remarks>This can typically be written as <c>&lt;@ CommandBase().RaiseCanExecuteChanged("") @&gt;</c></remarks>
+    abstract RaiseCanExecuteChangedCallQuotation : Expr<unit>
+    
+
+type Point = {
+    [<Range(10.0, 1000.0, ErrorMessage = "Value for {0} must be between {1} and {2}.")>] X: float;
+    Y: float;
+    [<StringLength(5, ErrorMessage = "The Str value cannot exceed 5 characters.")>] Str: string;
+}
+
