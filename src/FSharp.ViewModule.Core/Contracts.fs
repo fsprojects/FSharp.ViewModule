56f1cc796aba5918d4d31e0ef49476614523713f
ReedCopsey <reed@reedcopsey.com>
2014-01-01 16:36:38 -0800

    Updated proposed contracts

--- src/FSharp.ViewModule.Core/Contracts.fs
+++ src/FSharp.ViewModule.Core/Contracts.fs
@@ -12,14 +12,21 @@ type INotifyCommand =
     abstract RaiseCanExecuteChanged : unit -> unit
 
 
+/// Results of a validation for the member of a type
+type ValidationResult = { MemberName : string ; Errors : string seq }
+
+
 /// <summary>Extension of INotifyPropertyChanged with a public method to fire the PropertyChanged event</summary>
 /// <remarks>This type should provide a constructor which accepts an Execute (obj -> unit) and CanExecute (obj -> bool) function</remarks>
-type IViewModel<'a> =
+type IViewModel =
     inherit INotifyPropertyChanged
     inherit INotifyDataErrorInfo
 
     /// Trigger the PropertyChanged event for this specific ViewModel
     abstract RaisePropertyChanged : string -> unit
-
+    
     /// Value used to notify view that an asynchronous operation is executing
-    abstract Executing : bool
+    abstract OperationExecuting : bool with get, set
+
+    /// Setup all errors for validation
+    abstract SetErrors : ValidationResult seq -> unit
--- src/FSharp.ViewModule.Core/IViewModuleTypeSpecification.fs
+++ src/FSharp.ViewModule.Core/IViewModuleTypeSpecification.fs
@@ -7,22 +7,10 @@ open System.ComponentModel
 
 /// The Specification used by the type provider to generate a view model
 type IViewModuleTypeSpecification =
-    /// The type used for the View Model.
+
+    /// The type used for the View Model.  Should implement IViewModel
     abstract ViewModelType : System.Type
-    /// <summary>A quotation used to build the method to raise PropertyChanged on the ViewModel. </summary>
-    /// <remarks>This can typically be written as <c>&lt;@ ViewModelBase().RaisePropertyChanged("") @&gt;</c></remarks>
-    abstract RaisePropertyChangedCallQuotation : Expr<unit>
     
-    /// The type used for implementing ICommand
+    /// The type used for implementing ICommand.  Should implement INotifyCommand
     abstract CommandType : System.Type
-    /// <summary>A quotation used to build the method to raise CanExecuteChanged on a Command. </summary>
-    /// <remarks>This can typically be written as <c>&lt;@ CommandBase().RaiseCanExecuteChanged("") @&gt;</c></remarks>
-    abstract RaiseCanExecuteChangedCallQuotation : Expr<unit>
     
-
-type Point = {
-    [<Range(10.0, 1000.0, ErrorMessage = "Value for {0} must be between {1} and {2}.")>] X: float;
-    Y: float;
-    [<StringLength(5, ErrorMessage = "The Str value cannot exceed 5 characters.")>] Str: string;
-}
-
