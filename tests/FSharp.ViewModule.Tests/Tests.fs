6898c279511daecc137adfba3c09b5ad664733ba
ReedCopsey <reed@reedcopsey.com>
2014-01-03 16:56:42 -0800

    Minor updates to tests

--- tests/FSharp.ViewModule.Tests/Tests.fs
+++ tests/FSharp.ViewModule.Tests/Tests.fs
@@ -1,4 +1,4 @@
-﻿module FSharp.ViewModule.Tests
+﻿namespace FSharp.ViewModule.Tests
 
 open NUnit.Framework
 open NUnit.Framework.Constraints
@@ -6,30 +6,33 @@ open FsUnit
 open FSharp.ViewModule
 open System.ComponentModel
 
+// Builds ViewModels based on "Model" assembly, using "MvvmCross" based classes as the base classes
 type ViewModels = ViewModelProvider<"FSharp.ViewModule.Tests.Model", "FSharp.ViewModule.MvvmCross", "FSharp.ViewModule.MvvmCross.ViewModuleTypeSpecification">
 
-[<Test>]
-let ``Can create an instance of Home ViewModule`` () =
-  let home = ViewModels.Home()
-  home.Fullname |> should equal " "  
+module SpecificTests =
+    [<Test>]
+    let ``Can create an instance of Home ViewModule`` () =
+        let home = ViewModels.Home()
+        home.Fullname |> should equal " "  
 
-[<Test>]
-let ``Setting names in Home ViewModule should raise Property Changed`` () =
-  let home = ViewModels.Home()
-  home.ShouldAlwaysRaiseInpcOnUserInterfaceThread(false) // Required for MvvmCross to not delay the prop changed events
-  let resArr = ResizeArray<string>()
-  use subscription = home.PropertyChanged.Subscribe(fun args -> resArr.Add(args.PropertyName))
-  home.Firstname <- "Foo"
-  home.Lastname <- "Bar"
+    [<Test>]
+    let ``Setting names in Home ViewModule should raise Property Changed`` () =
+        let home = ViewModels.Home()
+        home.ShouldAlwaysRaiseInpcOnUserInterfaceThread(false) // Required for MvvmCross to not delay the prop changed events
+        let resArr = ResizeArray<string>()
+        use subscription = home.PropertyChanged.Subscribe(fun args -> resArr.Add(args.PropertyName))
+        home.Firstname <- "Foo"
+        home.Lastname <- "Bar"
 
-  resArr.Count |> should be (greaterThanOrEqualTo 4)
-  resArr |> should contain "Firstname"
-  resArr |> should contain "Lastname"
-  resArr |> should contain "Fullname"
+        resArr.Count |> should be (greaterThanOrEqualTo 4)
+        resArr |> should contain "Firstname"
+        resArr |> should contain "Lastname"
+        resArr |> should contain "Fullname"
 
-[<Test>]
-let ``Click in command should increment ClickCount`` () =
-    let home = ViewModels.Home()
-    home.ClickCount |> should equal 0
-    home.Click.Execute(null)
-    home.ClickCount |> should equal 1
+    [<Test>]
+    let ``Click in command should increment ClickCount`` () =
+        let home = ViewModels.Home()
+        home.ClickCount |> should equal 0
+        home.Click.Execute(null)
+        home.ClickCount |> should equal 1
+
