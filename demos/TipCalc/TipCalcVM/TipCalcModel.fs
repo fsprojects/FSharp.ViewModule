0de21dbff3a75bf686f17cf408d58072fa00cb2a
ReedCopsey <reed@reedcopsey.com>
2014-07-30 13:38:11 -0700

    Added TipCalc demo for Xamarin.Forms
    Ported from Charles Petzold's Xamarin.Forms C# sample project.

---
+++ demos/TipCalc/TipCalcVM/TipCalcModel.fs
@@ -0,0 +1,26 @@
+﻿namespace TipCalc
+
+open FSharp.ViewModule
+open System
+
+type TipCalcModel() as self =
+    inherit ViewModelBase()
+
+    // Create our backing fields
+    let subTotal = self.Factory.Backing(<@ self.SubTotal @>, 0.0)
+    let postTaxTotal = self.Factory.Backing(<@ self.PostTaxTotal @>, 0.0)
+    let tipPercent = self.Factory.Backing(<@ self.TipPercent @>, 15.0)
+
+    // Add dependencies for INotifyPropertyChanged to be handled correctly
+    do
+        self.DependencyTracker.AddPropertyDependencies(<@ self.TipAmount @>, [ <@@ self.SubTotal @@> ; <@@ self.PostTaxTotal @@> ; <@@ self.TipPercent @@>])
+        self.DependencyTracker.AddPropertyDependencies(<@ self.Total @>, [ <@@ self.SubTotal @@> ; <@@ self.PostTaxTotal @@> ; <@@ self.TipPercent @@>])
+    
+    member this.SubTotal with get() = subTotal.Value and set(v) = subTotal.Value <- v
+    member this.PostTaxTotal with get() = postTaxTotal.Value and set(v) = postTaxTotal.Value <- v
+    member this.TipPercent with get() = tipPercent.Value and set(v) = tipPercent.Value <- v
+
+    member this.TipAmount = Math.Round(this.TipPercent * this.SubTotal / 100.0, 2)
+
+    /// Total value, rounded to nearest quarter.
+    member this.Total = Math.Round(4.0 * (this.PostTaxTotal + this.TipAmount)) / 4.0;
