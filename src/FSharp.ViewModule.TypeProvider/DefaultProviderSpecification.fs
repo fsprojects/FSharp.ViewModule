d879aaed172ad9a86ea6e1a1c0dbd914fd235e33
ReedCopsey <reed@reedcopsey.com>
2015-12-08 17:40:02 -0800

    Removed type provider
    Removed the non-maintained type provider

--- src/FSharp.ViewModule.TypeProvider/DefaultProviderSpecification.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace FSharp.ViewModule.TypeProvider
-
-type DefaultViewModuleTypeSpecification() =
-    interface FSharp.ViewModule.IViewModuleTypeSpecification with
-        member this.ViewModelType = typeof<FSharp.ViewModule.ViewModelBase>
-        member this.CommandType = typeof<FSharp.ViewModule.FunCommand>
-        member this.Platform = { Framework = ".NET" }
-
-
