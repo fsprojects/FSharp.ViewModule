cb0725678027642ff4fae55de319fb87279bfa1c
Reed Copsey, Jr <reed@reedcopsey.com>
2015-12-10 11:34:21 -0800

    Fixed version info

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,3 @@
-#### 0.9.2 - December 10 2015
+#### 0.9.9.2 - December 10 2015
 * Removed type provider
 * Moved to ProjectScaffold for new structure and build system
--- src/FSharp.ViewModule/AssemblyInfo.fs
+++ src/FSharp.ViewModule/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.ViewModule")>]
 [<assembly: AssemblyProductAttribute("FSharp.ViewModule")>]
 [<assembly: AssemblyDescriptionAttribute("Library providing MVVM and INotifyPropertyChanged support for F# projects")>]
-[<assembly: AssemblyVersionAttribute("0.9.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.2")>]
+[<assembly: AssemblyVersionAttribute("0.9.9.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.9.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.2"
+    let [<Literal>] Version = "0.9.9.2"
