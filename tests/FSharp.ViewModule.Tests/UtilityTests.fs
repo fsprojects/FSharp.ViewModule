9563de09e71471b17ed7f56413e8516878e48c94
ReedCopsey <reed@reedcopsey.com>
2015-12-10 17:19:44 -0800

    More utility testing

--- tests/FSharp.ViewModule.Tests/UtilityTests.fs
+++ tests/FSharp.ViewModule.Tests/UtilityTests.fs
@@ -1,4 +1,4 @@
-module FSharp.ViewModule.Tests
+module FSharp.ViewModule.Tests.Utilities
 
 open FSharp.ViewModule
 open NUnit.Framework
@@ -8,3 +8,30 @@ let ``castAs unboxes successfully`` () =
   let initial : obj = box <| ResizeArray<int>()
   let cast : ResizeArray<int> = castAs<ResizeArray<int>>(initial)
   Assert.IsNotNull(cast)
+
+[<Test>]
+let ``castAs returns null when provided null`` () =
+  let initial : obj = null
+  let cast : ResizeArray<float> = castAs<ResizeArray<float>>(initial)
+  Assert.IsNull(cast)
+
+[<Test>]
+let ``castAs returns null when type doesn't match`` () =
+  let initial : obj = box <| ResizeArray<int>()
+  let cast : ResizeArray<float> = castAs<ResizeArray<float>>(initial)
+  Assert.IsNull(cast)
+
+type Test() =
+    member val Prop = 42 with get, set
+
+[<Test>]
+let ``getPropertyNameFromExpression returns correct name`` () =
+  let inst = Test()
+  let name = getPropertyNameFromExpression(<@ inst.Prop @>)
+  Assert.AreEqual("Prop", name)
+
+[<Test>]
+let ``Helpers getPropertyNameFromExpression returns correct name`` () =
+  let inst = Test()
+  let name = Helpers.getPropertyNameFromExpression(<@ inst.Prop @>)
+  Assert.AreEqual("Prop", name)
