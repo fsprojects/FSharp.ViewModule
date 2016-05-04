f3186a52f80c5216d8009470afded2dcff2b3c0a
ReedCopsey <reed@reedcopsey.com>
2016-05-04 16:25:46 -0700

    Updated version to 0.9.9.3

--- src/FSharp.ViewModule/AssemblyInfo.fs
+++ src/FSharp.ViewModule/AssemblyInfo.fs
@@ -4,7 +4,7 @@ open System.Reflection
 open System.Runtime.CompilerServices
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.9.2"
+    let [<Literal>] Version = "0.9.9.3"
 
 [<assembly: AssemblyTitleAttribute("FSharp.ViewModule")>]
 [<assembly: AssemblyProductAttribute("FSharp.ViewModule")>]
